dd5b7789e0a224c08d7c86e34e33e4f1d60785f1
Arlen Neylon <aneylon@scramsystems.com>
2023-04-12 16:11:07 -0600

    Add tools section and random name generator.

---
+++ src/Components/Tools/Tools.js
@@ -0,0 +1,11 @@
+import RandomNameGenerator from "./RandomNameGenerator";
+
+export const Tools = () => {
+  return (
+    <div>
+      <h1>Tools</h1>
+      <RandomNameGenerator />
+      <p>Moar things here...</p>
+    </div>
+  );
+};
