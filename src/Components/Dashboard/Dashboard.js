0f3053aed51565af9040c1d03898a668d270b563
Arlen Neylon <aneylon@scramsystems.com>
2023-04-20 11:38:43 -0600

    Start adding routing.

---
+++ src/Components/Dashboard/Dashboard.js
@@ -0,0 +1,13 @@
+import { News } from "../News/News";
+import { Tools } from "../Tools/Tools";
+
+export const Dashboard = () => {
+  return (
+    <>
+      <h1>Dashboard!</h1>
+
+      <News />
+      <Tools />
+    </>
+  );
+};
