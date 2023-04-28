8ddc3c46404e47dc91a5c9ddf39241592382d38f
Arlen Neylon <aneylon@scramsystems.com>
2023-04-28 16:46:58 -0600

    Add app bar to header.

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -1,20 +1,60 @@
+import {
+  AppBar,
+  Button,
+  Container,
+  IconButton,
+  Toolbar,
+  Typography,
+} from "@mui/material";
 import { Link, Outlet } from "react-router-dom";
-
+import MenuIcon from "@mui/icons-material/Menu";
 export const Header = () => {
   return (
     <>
-      <nav>
-        <h1>HiveMind</h1>
-        <Link to="/">Dashboard</Link>
-        <Link to="tools">Tools</Link>
-        <Link to="about">About</Link>
-        <Link to="faq">FAQ</Link>
-        <Link to="admin">Admin</Link>
-        <Link to="signIn">SignIn</Link>
-        <Link to="signUp">SignUp</Link>
-        <span>SignOut</span>
-      </nav>
-      <Outlet />
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h4" sx={{ flexGrow: 1 }}>
+            HiveMind
+          </Typography>
+
+          <nav>
+            <Button component={Link} to="/" color="inherit">
+              Dashboard
+            </Button>
+            <Button component={Link} to="tools" color="inherit">
+              Tools
+            </Button>
+            <Button component={Link} to="about" color="inherit">
+              About
+            </Button>
+            <Button component={Link} to="faq" color="inherit">
+              FAQ
+            </Button>
+            <Button component={Link} to="admin" color="inherit">
+              Admin
+            </Button>
+            <Button component={Link} to="signIn" color="inherit">
+              SignIn
+            </Button>
+            <Button component={Link} to="signUp" color="inherit">
+              SignUp
+            </Button>
+            <Button color="inherit">SignOut</Button>
+          </nav>
+        </Toolbar>
+      </AppBar>
+      <Container>
+        <Outlet />
+      </Container>
     </>
   );
 };
