1b0070ed515939f54dd5b37a528bd70eb6045e8b
Arlen Neylon <aneylon@scramsystems.com>
2023-05-08 11:24:36 -0600

    Add color scheme generator.

--- src/Components/Tools/Tools.js
+++ src/Components/Tools/Tools.js
@@ -1,4 +1,5 @@
 import { CharacterGenerator } from "../ShadowDark/CharacterGenerator";
+import ColorScheme from "./ColorScheme";
 import Dice from "./Dice";
 import RandomNameGenerator from "./RandomNameGenerator";
 
@@ -6,6 +7,7 @@ export const Tools = () => {
   return (
     <div>
       <h1>Tools</h1>
+      <ColorScheme />
       <Dice />
       <CharacterGenerator />
       <RandomNameGenerator />
