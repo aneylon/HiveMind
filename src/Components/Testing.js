52b033a272efd6dafe90a21ca9f144c22ee64d88
aneylon <arlen.m.neylon@gmail.com>
2023-05-21 19:31:39 -0600

    Set up route for testing css.

---
+++ src/Components/Testing.js
@@ -0,0 +1,17 @@
+const Testing = () => {
+  return (
+    <div>
+      <h1>Testing</h1>
+    </div>
+  );
+};
+export default Testing;
+
+/* References
+https://codepen.io/luxonauta/pen/PoNOoqB
+https://codepen.io/jcubic/pen/BwBYOZ
+https://medium.com/@benjamib/retro-terminal-ui-ae9ac8eae71a
+https://dev.to/ekeijl/retro-crt-terminal-screen-in-css-js-4afh
+https://css-tricks.com/old-timey-terminal-styling/
+https://codesandbox.io/s/crt-terminal-in-css-js-forked-l9nerw
+*/
